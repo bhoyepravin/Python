d0a60ba26fc0b08fd98b0e3fe1df0934e49d8082
YatinDevs <c.yatin727@gmail.com>
2025-07-08 21:47:33 +0530

    python learning

---
+++ 01_python/01_hello.py
@@ -0,0 +1,6 @@
+print("Hello world")
+
+# This is a simple Python script that prints "Hello world" to the console.# It serves as a basic introduction to Python syntax and execution.
+# To run this script, you can use the command line or terminal:
+# 1. Open your terminal or command prompt.
+
